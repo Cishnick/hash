b7ed70ebc2c063c5c3fb678233a00b8226d0f020
nick <cishnick.yandex.ru>
2022-12-19 15:27:54 +0700

    simple implementation of chain has table has been completed

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,91 @@
 #include <iostream>
+#include <list>
+#include <algorithm>
+#include <vector>
 
+
+
+class ChainHash {
+public:
+    ChainHash(size_t size) : 
+        capacity(size),
+        _count(0)
+    {
+        table = new std::list<std::string>[capacity];
+    }
+
+    ~ChainHash() {
+        delete[] table;
+    }
+
+    void add_item(std::string const& item) {
+        _count++;
+        auto key = hash_func(item);
+        table[key].push_back(item);
+    }
+
+    void remove_item(std::string const& item) {
+        _count--;
+        auto key = hash_func(item);
+        table[key].remove(item);
+    }
+
+    size_t count() const {
+        return _count;
+    }
+
+    bool containing(std::string const& item) const {
+        auto key = hash_func(item);
+        auto chain = table[key];
+        return std::find(chain.cbegin(), chain.cend(), item) != chain.cend();
+    }
+
+    void display() {
+        using namespace std;
+        for(int i = 0; i != capacity; ++i) {
+            cout << i;
+            for(auto const& x : table[i]) {
+                cout << " --> " << x;
+            }
+            cout << endl;
+        }
+        cout << "count: " << count() << endl;
+    }
+
+private:
+    using hash_t = unsigned long;
+
+    hash_t hash_func(std::string const& data) const{
+        hash_t i = 0;
+        for(auto sym : data)
+            i += sym;
+        return i % capacity;
+    }
+
+    std::list<std::string> *table;
+    const size_t capacity = 10000;
+    size_t _count;
+};
 
 
 int main() {
-    std::cout << "Hello World!!!" << std::endl;
+
+    std::vector<std::string> strs{"Hello world!", "Hello dlrow!", "Python is shit", "Linux Manjaro"};
+    ChainHash hash(10);
+
+    hash.add_item(strs[0]);
+    hash.add_item(strs[1]);
+    hash.add_item(strs[2]);
+    hash.add_item(strs[3]);
+
+    hash.display();
+    std::cout << std::endl;
+
+
+    hash.remove_item(strs[0]);
+    hash.remove_item(strs[3]);
+
+    hash.display();
+
     return 0;
 }
