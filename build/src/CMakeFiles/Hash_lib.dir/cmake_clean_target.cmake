00227333aece03c13b6aafbf02eec70b2019d2b1
Cyshnick <cishnick@yandex.ru>
2023-01-03 15:43:00 +0700

    update

--- build/src/CMakeFiles/Hash_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libHash_lib.a"
-)
--- build/tst/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/myCode/Hash/hash/tst
-# Build directory: C:/myCode/Hash/build/tst
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Hash_tstg "Hash_tst")
-set_tests_properties(Hash_tstg PROPERTIES  _BACKTRACE_TRIPLES "C:/myCode/Hash/hash/tst/CMakeLists.txt;26;add_test;C:/myCode/Hash/hash/tst/CMakeLists.txt;0;")
-subdirs("../_deps/googletest-build")
