c07f7b311976e850d5d44ae58e1d861b6079fab1
Cyshnick <cishnick@yandex.ru>
2023-01-03 15:30:44 +0700

    init

---
+++ build/src/CMakeFiles/Hash_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libHash_lib.a"
+)
---
+++ build/tst/CMakeFiles/Hash_tst.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hash_tst.dir/main_test.cc.obj"
+  "CMakeFiles/Hash_tst.dir/main_test.cc.obj.d"
+  "Hash_tst.exe"
+  "Hash_tst.exe.manifest"
+  "Hash_tst.pdb"
+  "libHash_tst.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hash_tst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
